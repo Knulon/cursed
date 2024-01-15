6b1a37b272e9080d0468a2c0881c5ea9a3144313
tobiasf <30748375+tobiasfrck@users.noreply.github.com>
2024-01-15 19:00:21 +0100

    gamemanager changes + textBox finished except design

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -12,8 +12,8 @@ public class GameManager : MonoBehaviour
     [SerializeField] 
     private GameObject _enemySpawnpointPrefab;
 
-    private Dictionary<int, List<GameObject>> _playerSpawnpoints;
-    private Dictionary<int, List<GameObject>> _keyLocations;
+    private Dictionary<int, GameObject> _playerSpawnpoints;
+    private Dictionary<int, GameObject> _keyLocations;
     private Dictionary<int,List<GameObject>> _enemySpawnpointsPerLevel;
 
     // Start is called before the first frame update
@@ -21,11 +21,11 @@ public class GameManager : MonoBehaviour
     {
         _player = GameObject.Find("Player");
         _playerSpawnpoints = new();
-        FillDictionaryWithGameObjectLists(_playerSpawnpoints, "PlayerSpawnpoint"); //TODO: remove list
+        FillDictionaryWithGameObjects(_playerSpawnpoints, "PlayerSpawnpoint");
         _enemySpawnpointsPerLevel = new();
         FillDictionaryWithGameObjectLists(_enemySpawnpointsPerLevel, "EnemySpawnpoint");
         _keyLocations = new();
-        FillDictionaryWithGameObjectLists(_keyLocations, "KeyLocation"); //TODO: remove list
+        FillDictionaryWithGameObjects(_keyLocations, "KeyLocation");
 
         // Level Objects are named like "PlayerSpawnpoint_1" or "EnemySpawnpoint_1" or "ExitTrigger_1" with the number being the nextLevelID
 
@@ -48,10 +48,20 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    private void FillDictionaryWithGameObjects(Dictionary<int, GameObject> dictionary, string tag)
+    {
+        List<GameObject> gameObjects = new(GameObject.FindGameObjectsWithTag(tag));
+        foreach (var gameObject in gameObjects)
+        {
+            var level = Convert.ToInt32(gameObject.name.Split('_')[1]);
+            dictionary.Add(level, gameObject);
+        }
+    }
+
     public void nextLevel(int nextLevelID)
     {
         ClearLevel();
-        _player.transform.position = _playerSpawnpoints[nextLevelID][Random.Range(0,_playerSpawnpoints.Count)].transform.position;
+        _player.transform.position = _playerSpawnpoints[nextLevelID].transform.position;
 
         foreach (var enemySpawnpoint in _enemySpawnpointsPerLevel[nextLevelID])
         {
--- Assets/TextAppear.cs
+++ Assets/TextAppear.cs
@@ -9,6 +9,13 @@ public class TextAppear : MonoBehaviour
     [SerializeField]
     public float timeToDisplayFullText = 1f; // in seconds
 
+    [SerializeField]
+    public float timeToDisplayCharacter = 0.1f; // in seconds
+
+    [SerializeField]
+    [Tooltip("Check this if you want to set how long it takes until the next character is displayed.")]
+    public bool timePerCharacter = false; // if true, timeToDisplayFullText is ignored and timeToDisplayCharacter is used instead
+
     private float timePassed = 0f;
 
     private String textToDisplay = "Hello World!";
@@ -37,9 +44,14 @@ public class TextAppear : MonoBehaviour
             Appear();
 
 
-        if (timePassed < timeToDisplayFullText)
+        if (!timePerCharacter && timePassed < timeToDisplayFullText)
         {
             textMesh.text = textToDisplay.Substring(0, (int)(timePassed / timeToDisplayFullText * textToDisplay.Length));
+        }
+
+        if (timePerCharacter && timePassed < timeToDisplayCharacter * textToDisplay.Length)
+        {
+            textMesh.text = textToDisplay.Substring(0, (int)(timePassed / timeToDisplayCharacter));
         }
 
         timePassed += Time.deltaTime;
